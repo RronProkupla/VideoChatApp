b851a34d4f2f580fea56ea46ca585a1327dff5b0
RronProkupla <rronir2@gmail.com>
2020-03-27 02:00:00 +0100

    UI and chat

--- public/script.js
+++ public/script.js
@@ -59,3 +59,46 @@ const addVideoStream = (video,stream) => {
 
     videoGrid.append(video)
 }
+
+const cameraToggle = () => {
+    document.querySelector('.camera').innerHTML = "hello"
+}
+
+const micToggle = () => {
+    const on  = '<i class="fa fa-microphone fa-2x" style="color: white;" aria-hidden="true"></i>'
+    const off = '<i class="fa fa-microphone-slash fa-2x" style="color: white;" aria-hidden="true"></i>'
+    if(document.querySelector('.microphone').innerHTML == on){
+        document.querySelector('.microphone').innerHTML = off
+    }else{
+        document.querySelector('.microphone').innerHTML = on
+    }
+    
+}
+
+let input = document.querySelector('#messageinput')
+
+input.addEventListener('keydown', (e) => {
+    if(e.keyCode == 13 && input.value != 0 ){
+        console.log(input.value)
+        socket.emit('message', input.value)
+        input.value = ''
+    }
+})
+
+
+socket.on('deliverMessage', (message,userId) => {
+    document.querySelector('.messages').appendChild(createSingleMessage(message,userId))
+})
+
+const createSingleMessage = (message,userId) => {
+    let li = document.createElement('li')
+    li.setAttribute("class", "singleMessage")
+    li.textContent = `${userId}  ${message}`
+    return li
+}
+
+
+
+const scrolldiv = document.querySelector('.messages')
+
+scrolldiv.scrollTop = scrolldiv.scrollHeight
